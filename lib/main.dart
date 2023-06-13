ed765a80d76b8da1823c6befff951ffb3e7d7993
Muhammad Salman Ahmad <salman.ahmad1@arbisoft.com>
2023-06-13 17:53:52 +0500

    modifiled app/build.gradle file

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:air_feed/onboarding_Screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp( const MaterialApp(home: OnboardingScreen(),));
 }
 
